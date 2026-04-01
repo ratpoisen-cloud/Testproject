eb40bbd8cb904bb37d57fc12571447c04fdf481a
ratpoisen-cloud <ratpoisen@gmail.com>
2026-04-02 00:32:02 +0300

    Update board-ui.js

--- js/board-ui.js
+++ js/board-ui.js
@@ -5,9 +5,7 @@ window.PIECE_SET_STORAGE_KEY = 'chess-piece-set';
 window.DEFAULT_PIECE_SET = 'cdn';
 window.PIECE_SETS = {
     cdn: { label: 'Стандартные (CDN)', theme: 'https://chessboardjs.com/img/chesspieces/alpha/{piece}.png' },
-    custom: { label: 'chessnut', theme: 'assets/pieces/custom/{piece}.svg' },
-    custom: { label: '2', theme: 'assets/pieces/custom/{piece}.svg' },
-    custom: { label: '3', theme: 'assets/pieces/custom/{piece}.svg' }
+    custom: { label: 'chessnut', theme: 'assets/pieces/custom/{piece}.svg' }
 };
 
 window.getCurrentPieceTheme = function() {
